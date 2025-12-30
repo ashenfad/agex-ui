4c8986a7752be7aff32dc58551ce3a2cddc5a906
Adam Ashenfelter <ashenfad@gmail.com>
2025-12-30 11:22:44 -0800

    chore: leftovers

--- agex_ui/cal/agent.py
+++ agex_ui/cal/agent.py
@@ -1,7 +1,7 @@
 import calgebra
 import calgebra.gcsa as gcsa
 import calgebra.mutable as mutable
-from agex import Agent, Versioned, connect_llm, connect_state, connect_host, events
+from agex import Agent, Versioned, connect_llm, connect_state
 from agex.helpers import register_pandas, register_plotly, register_stdlib
 from calgebra.gcsa import Event
 
